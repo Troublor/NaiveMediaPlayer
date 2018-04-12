59ba0701592afb7f274db1b7cd2ad80b9320137e
Troublor <troublor@live.com>
2018-04-12 17:48:24 +0800

    able to download music

--- NaiveMediaPlayer/PlayPage.xaml.cs
+++ NaiveMediaPlayer/PlayPage.xaml.cs
@@ -13,8 +13,13 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Collections.ObjectModel;
+using Windows.Media.Core;
+using Windows.Networking.BackgroundTransfer;
 using Windows.Storage;
+using Windows.Storage.Search;
+using Windows.Storage.Streams;
 using Windows.System.Display;
+using Windows.UI.Popups;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -28,9 +33,29 @@ namespace NaiveMediaPlayer
     {
         private ObservableCollection<PlayHistory> _playHistory = new ObservableCollection<PlayHistory>();
 
+        private StorageFolder _saveFolder;
+
         public PlayPage()
         {
             this.InitializeComponent();
+            this.GetOrCreateFolder();
+
+        }
+
+        private async void GetOrCreateFolder()
+        {
+            var myMusic = await Windows.Storage.StorageLibrary.GetLibraryAsync
+                (Windows.Storage.KnownLibraryId.Music);
+            Windows.Storage.StorageFolder saveMusicFolder = myMusic.SaveFolder;
+            try
+            {
+                this._saveFolder = await StorageFolder.GetFolderFromPathAsync(saveMusicFolder.Path + @"\naive_media");
+            }
+            catch (Exception e)
+            {
+                this._saveFolder = await saveMusicFolder.CreateFolderAsync("naive_media");
+            }
+            test();
         }
 
         private async void OnChooseFileButtonClicked(object sender, RoutedEventArgs e)
@@ -49,10 +74,10 @@ namespace NaiveMediaPlayer
                 // Application now has read/write access to the picked file
                 var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
                 MediaElement.SetSource(stream, file.ContentType);
-                MediaElement.Play();      
+                MediaElement.Play();
             }
-        
-          
+
+
         }
         private DisplayRequest appDisplayRequest = null;
 
@@ -80,6 +105,46 @@ namespace NaiveMediaPlayer
                     }
                 }
             }
+        }
+
+        private async void test()
+        {
+            //            StorageFolder testFolder = await StorageFolder.GetFolderFromPathAsync(@"C:\Users\troub\Pictures");
+            //            StorageFile sourceFile = await testFolder.GetFileAsync("选课.PNG");
+            //            StorageFile destinationFile = await KnownFolders.CameraRoll.CreateFileAsync("MyTestImage.png");
+            //
+            //            using (var sourceStream = await sourceFile.OpenReadAsync())
+            //            {
+            //                using (var sourceInputStream = sourceStream.GetInputStreamAt(0))
+            //                {
+            //                    using (var destinationStream = await destinationFile.OpenAsync(FileAccessMode.ReadWrite))
+            //                    {
+            //                        using (var destinationOutputStream = destinationStream.GetOutputStreamAt(0))
+            //                        {
+            //                            await RandomAccessStream.CopyAndCloseAsync(sourceInputStream, destinationStream);
+            //                        }
+            //                    }
+            //                }
+            //            }
+
+            Uri source = new Uri("http://www.neu.edu.cn/indexsource/neusong.mp3");
+
+            StorageFile destinationFile =
+                await this._saveFolder.CreateFileAsync("NeuSong.mp3", CreationCollisionOption.GenerateUniqueName);
+
+
+            BackgroundDownloader downloader = new BackgroundDownloader();
+            DownloadOperation download = downloader.CreateDownload(source, destinationFile);
+
+            await download.StartAsync();
+
+        }
+
+        private void MenuFlyoutItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            MediaElement.Source = new Uri("http://www.neu.edu.cn/indexsource/neusong.mp3");
+            MediaElement.Play();
+            _playHistory.Add(new PlayHistory("neusong.mp3", "http://www.neu.edu.cn/indexsource/neusong.mp3"));
         }
     }
 
